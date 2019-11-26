2c2b596433b3bfa4156b12ee3fe05ed2a7bf46a4
Maxime <mantoine1@myges.fr>
2019-11-26 16:31:04 +0100

    nav basique

--- template-method/src/app/app.module.ts
+++ template-method/src/app/app.module.ts
@@ -3,14 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HamtaroComponent } from './hamtaro/hamtaro.component';
+import { RhinoferosComponent } from './rhinoferos/rhinoferos.component';
+import { Routes, RouterModule } from '@angular/router';
+import { HeaderMenuComponent } from './header-menu/header-menu.component';
+
+const appRoutes: Routes = [
+  { path: 'hamtaro', component: HamtaroComponent },
+  { path: 'rhinoferos', component: RhinoferosComponent },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HamtaroComponent,
+    RhinoferosComponent,
+    HeaderMenuComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
